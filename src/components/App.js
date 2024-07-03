4c4a79b2847cbf0d0b8cde00c30240fe226e120a
hiboherson <hiboherson@gmail.com>
2024-07-03 09:33:20 -0700

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "../components/Header"
+import About from "../components/About";
+import ArticleList from "../components/ArticleList"
+import Article from "../components/Article";
+
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck! */}
+    <Header name={blogData.name}/>
+    <About image={blogData.image} about={blogData.about}/>
+    <ArticleList posts={blogData.posts}  />
+    <Article title={blogData.title}/>
     </div>
   );
 }
